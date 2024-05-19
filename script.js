aa0027b60c0e31420a0e61812fafa3bf083b4f5c
Eita-jesus <thequeira@email.com>
2024-05-19 12:41:08 -0300

    Ajustes nas descricoes - comentários do código

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Em Java Script é uma boa prática iniciar as variaveis com zero ou vario;
+// Em Java Script é uma boa prática iniciar as variaveis com zero ou vazio, além disso é sempre importante declarar elas no inicio do código;
 
 let ruuningTotal = 0;
 let buffer = "0";
@@ -6,8 +6,21 @@ let previousOperador ;
 
 const screen = document.querySelector('.screen')
 
+//Primeiro passo foi criar minha funcao ouvinte, ela vai no DOM e fica escutando ou aguardando o botão ser clicado.Após ser clicado ela vai chamar a funcao buttonClick e passar o valor dela.
+
+function init(){
+    document.querySelector('.calc-buttons').addEventListener('click', function(event){
+        buttonClick(event.target.innerText);
+    })
+}
+
+//Aqui estou apenas chamando essa funcão, como se tivesse deixando ela ligada (ATIVO)
+init();
 
 //Funcao resposavel por validar se é um numero ou um simbolo.
+//Recebo um valor e vejo é número ou não.
+//Depois que faco essa validacao o screen.innerText = buffer, fica quietinho para ser executado no final do código.
+
 function buttonClick(value){
     if(isNaN(value)){
         handleSymbol(value);
@@ -17,6 +30,8 @@ function buttonClick(value){
     screen.innerText = buffer;
 }
 
+
+//Caso seja um Simbolo vamos realizar a operacao.
 function handleSymbol(symbol){
 
     switch(symbol){
@@ -28,7 +43,9 @@ function handleSymbol(symbol){
             if(previousOperador === null){
                 return
             }
+            //Aqui é uma chamada da funcao que realiza a operacao
             flushOperation(parseInt(buffer));
+            //Depois que realizo minhas operacoes matematicas atribuo zero nos metodos. 
             previousOperador = null;
             buffer = ruuningTotal;
             ruuningTotal = 0;
@@ -50,6 +67,8 @@ function handleSymbol(symbol){
     }
 }
 
+
+    //Controlador, responsavel por antes de realizar a operacao matemática, ver se o valor das parcela seria zero ou nao, caso seja zero nao ha operacao matemática tenha um valor vai chamar a funcao por realixar o calculo matematico
     function handleMath(symbol){
         if (buffer === '0'){
             return;
@@ -61,10 +80,11 @@ function handleSymbol(symbol){
         }else{
             flushOperation(intBuffer);
         }
-        previousOperador =symbol;
+        previousOperador = symbol;
         buffer = '0'
     }
 
+    //funcao responsável por realixar a operacao matematica
     function flushOperation(intBuffer){
         if (previousOperador === '+'){
             ruuningTotal += intBuffer;
@@ -77,21 +97,16 @@ function handleSymbol(symbol){
         }
     }
 
+    //Essa funcao é apenas um tratamento, imagina que digitassemos 1 em seguida 0, que é 10, se não ouvesse esse tratamento nao conseguiriamos tratar os numeros mais casas decimais.
     function handleNumber(numberString){
         if (buffer === "0"){
             buffer = numberString;
         }else{
+            //aqui é uma concatenacao de numeros no formato String 
             buffer += numberString;
+            //buffer = buffer + numberString
         }
     }
 
 
 
-function init(){
-    document.querySelector('.calc-buttons').addEventListener('click', function(event){
-        buttonClick(event.target.innerText);
-    })
-}
-
-
-init();
